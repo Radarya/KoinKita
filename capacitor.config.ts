dfc59942917cd976b56bd163dcb2045ed1001e82
RADITHYA AYDIN ARYASATYA <aydinaryasatyaradithya@gmail.com>
2026-07-09 00:03:47 +0700

    commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xyz.koinkita.app',
+  appName: 'Koin Kita',
+  webDir: 'dist'
+};
+
+export default config;
