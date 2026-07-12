6b327174053a01d029507879e22839f14622df5d
RADITHYA AYDIN ARYASATYA <aydinaryasatyaradithya@gmail.com>
2026-07-12 18:29:22 +0700

    chore: sync apk app name and version for TapTap

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'xyz.koinkita.app',
-  appName: 'Koin Kita',
+  appName: 'KoinKita',
   webDir: 'dist',
   plugins: {
     FirebaseAuthentication: {
